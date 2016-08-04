85494e002d4565d9ed7ef381dc7d0929dd4d8cf9
alisonberkowitz <alison.berkowitz@students.olin.edu>
2016-08-04 01:44:31 -0400

    add search and results views templates and routes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,11 @@ import static spark.Spark.*;
 import com.flatironschool.javacs.*;
 
 import redis.clients.jedis.Jedis;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -19,18 +22,27 @@ public class Main {
         Jedis jedis = JedisMaker.make();
         JedisIndex index = new JedisIndex(jedis);
 
-        get("/hello", (req, res) -> "Hello World");
-
-        // matches "GET /hello/foo" and "GET /hello/bar"
-        // request.params(":name") is 'foo' or 'bar'
-        get("/hello/:name", (request, response) -> {
-            return "Hello: " + request.params(":name");
-        });
-
         get("/search/:term", (req, res) -> {
             WikiSearch search = WikiSearch.search(req.params(":term"),index);
             List<Map.Entry<String, Integer>> entries = search.sort();
-            return entries;
-        });
+            Map map = new HashMap();
+            map.put("term", req.params(":term"));
+            map.put("entries", entries);
+            return new ModelAndView(map, "results.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/", (req, res) -> {
+            Map map = new HashMap();
+            return new ModelAndView(map, "search.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/search", (req, res) -> {
+            WikiSearch search = WikiSearch.search(req.queryParams("term"),index);
+            List<Map.Entry<String, Integer>> entries = search.sort();
+            Map map = new HashMap();
+            map.put("term", req.queryParams("term"));
+            map.put("entries", entries);
+            return new ModelAndView(map, "results.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
